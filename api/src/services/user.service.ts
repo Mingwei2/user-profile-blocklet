9040639df36bda3797dbba753e08a00c089de244
Mingwei Sun <mingwei.sun@thoughtworks.com>
2025-01-10 14:10:47 +0800

    feat: add type for user.

--- api/src/services/user.service.ts
+++ api/src/services/user.service.ts
@@ -1,9 +1,10 @@
 import db from '../libs/database';
+import { User } from '../types/user.type';
 
 export default class UserService {
-  static findById(id: string | undefined) {
+  static findById(id: string | undefined): User | null {
     if (!id) return null;
     const query = 'SELECT * FROM users WHERE id = ?';
-    return db.prepare(query).get(id);
+    return db.prepare(query).get(id) as User | null;
   }
 }
